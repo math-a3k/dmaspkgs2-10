776ce6218fab679acd07c21742f776aad19fdf1c
Rodrigo <rodrigo@crowdbotics.com>
2022-07-01 23:25:52 +0000

    Remove component Push Notifications (Django)

--- backend/modules/django_push_notifications/push_notifications/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PushNotificationsConfig(AppConfig):
-    name = "modules.django_push_notifications.push_notifications"
-    verbose_name = "Push Notifications"
