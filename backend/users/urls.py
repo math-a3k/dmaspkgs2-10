2551f6deb3819c4c2139474f6cfdf7b37b1b310c
math-a3k <rodrigo@crowdbotics.com>
2022-08-02 21:30:22 +0000

    Update models

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_update_view, user_redirect_view, user_detail_view, UserDetailView, UserUpdateView
+from .views import UserUpdateView, user_update_view, UserDetailView, user_detail_view, user_redirect_view
 
 app_name = "users"
 urlpatterns = [
