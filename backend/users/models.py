e9e9c11ff51f08cf7bae9558b4fff2aa711a0417
math-a3k <rodrigo@crowdbotics.com>
2022-08-02 21:21:46 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -19,10 +19,5 @@ class User(AbstractUser):
     everything that relates with an `User` is represented by this model.
     """
     name = models.CharField(null=True,blank=True,max_length=255,)
-    fffsssaa = models.BigIntegerField(null=True,blank=True,)
-    khjkj = models.BigIntegerField(null=True,blank=True,)
-    uuuiiuuu = models.BigIntegerField(null=True,blank=True,)
-    cderfv = models.BigIntegerField(null=True,blank=True,)
-    vcxz = models.BigIntegerField(null=True,blank=True,)
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_redirect_view, user_update_view, user_detail_view, UserUpdateView, UserDetailView
+from .views import user_update_view, user_redirect_view, user_detail_view, UserDetailView, UserUpdateView
 
 app_name = "users"
 urlpatterns = [
